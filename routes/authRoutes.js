6010468a5b82c7f119711e6531b708a3c99100c8
preciousenoch <preciousenoch459@gmail.com>
2025-08-01 10:59:32 +0100

    Add route for retrieving current user profile

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
-const { getCurrentUser } = require("../controllers/authController");
 
 router.get("/", (req, res) => {
     res.render("index");
@@ -13,6 +12,6 @@ router.post("/signup", authController.signup);
 router.get("/login", authController.loginPage);
 router.post("/login", authController.login);
 
-router.get("/profile", getCurrentUser);
+router.get("/profile",  authController.getCurrentUser);
 
 module.exports = router;
